4d4bbedd039b768884f679a7002088f249073532
CyrusNchege <cyruschegecc@gmail.com>
2023-04-08 12:38:35 +0300

    Add env example

--- accounts/models.py
+++ accounts/models.py
@@ -8,17 +8,8 @@ class User(AbstractUser):
 
 
 
-class Mentee(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, )
-    career_goal = models.CharField(max_length=255)
-    
-    def __str__(self):
-    	return self.user.username
-
-
-class Mentor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+class Mentee(User):
     career_goal = models.CharField(max_length=255)
 
-    def __str__(self):
-    	return self.user.username
+class Mentor(User):
+    career_goal = models.CharField(max_length=255)
