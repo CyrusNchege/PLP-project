f1ccca3ec580db0f67d8d94d16a04ec96610e664
CyrusNchege <cyruschegecc@gmail.com>
2023-05-04 00:02:46 +0300

    Refactoring password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/', views.loginpage, name = 'login'),
     path('register/', views.register, name = 'register'),
     path('logout/', views.logout_view, name = 'logout'),
+    path('password_reset/', views.password_reset, name = 'password_reset'),
 
 ]
--- community/settings.py
+++ community/settings.py
@@ -139,3 +139,12 @@ STATICFILES_DIRS = [BASE_DIR/'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+
