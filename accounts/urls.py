2b4f6fda6984d7cb88415afe8f539289188a5f58
CyrusNchege <cyruschegecc@gmail.com>
2023-05-08 13:15:33 +0300

    complete forgot password functionalities

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,10 +6,19 @@ urlpatterns = [
     path('login/', views.loginpage, name = 'login'),
     path('register/', views.register, name = 'register'),
     path('logout/', views.logout_view, name = 'logout'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name = 'password_reset'),
-    path ('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name = 'password_reset_done'),
-    path ('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name = 'password_reset_confirm'),
-    path ('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name = 'password_reset_complete'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+                template_name = 'accounts/password_reset.html'), 
+                name = 'password_reset'),
+    path ('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+                template_name = 'accounts/password_reset_done.html'),
+                 name = 'password_reset_done'),
+    path ('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+                template_name = 'accounts/password_reset_confirm.html'),
+                name = 'password_reset_confirm'),
+    path ('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+                template_name = 'accounts/password_reset_complete.html'), 
+                name = 'password_reset_complete'),
     
 
 ]
